37132eceb026f0ca77137c345e0c8a30c602daa7
uzoma <chidumaga@gmail.com>
2019-11-29 13:15:19 +0100

    adds layout component

---
+++ src/components/layout.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import {Helmet} from 'react-helmet'
+
+const Layout = (props) => {
+    return(
+        <React.Fragment>
+            <Helmet>
+                <meta charSet="utf-8" />
+                <title>Uzoma Orji | Visual Artist & Creative Technologist</title>
+                <meta name="description" content="Website of visual artist and creative technologist Uzoma Orji" />
+                <meta name="keywords" content="Uzoma, Orji, Uzoma Orji, Chidumaga, Visual Artist, 
+                Artist, Designer, Technologist, Creative, Nigeria, Africa" />
+                <meta name="author" content="Uzoma Orji" />
+                {/* <link rel="canonical" href="http://mysite.com/example" /> */}
+            </Helmet>
+            {props.children}
+        </React.Fragment>
+    )
+}
+
+export default Layout
