4fb2e12995c1ce25280fb0bc38cc7560fd76e0da
uzoma <chidumaga@gmail.com>
2020-01-17 00:22:06 +0100

    gallery carousel looking sweeeet

--- src/components/works.js
+++ src/components/works.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'gatsby'
 
 import Img from 'gatsby-image'
@@ -7,15 +7,22 @@ import workStyles from '../styles/work.module.css'
 
 const Works = ( { works } ) => {
 
+    const [width, setWidth] = useState('')
+    const maxWorkLength = 3
+
+    useEffect (() => {
+        works.length > maxWorkLength ? setWidth('350px') : setWidth('400px')
+    }, [works.length])
+
     return (
         <ul className={workStyles.works}>
             {
                 works.map((work, index) => 
                     <Link key={index} to={`/visuals/${work.slug}`}>
-                        <li>
+                        <li style={{width: width}}>
                             <Img fluid={work.images[0].fluid} alt='Uzoma Orji visual' />
-                            <p>{work.title}</p>
                         </li>
+                        <p className={workStyles.workTitle}>{work.title}</p>
                     </Link>
                 )
             }
