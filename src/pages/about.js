c2f7df11c4fcf356e150384cb1aa618279a4404d
uzoma <chidumaga@gmail.com>
2020-06-01 16:22:26 +0100

    adds about page text toggle and restyles buttons

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import aboutStyles from '../styles/about.module.css'
 
 import Layout from '../components/layout'
 
 const About = () => {
+
+    const [ isPartyVersion, setIsPartyVersion ] = useState(false)
+
+    const toggleText = isPartyVersion ?  'see the version i submit in applications' : 'see the version I say at parties'
 
     return (
         <Layout>
@@ -19,28 +23,71 @@ const About = () => {
                             <div className={aboutStyles.formCircle} style={{background: '#ffcccc'}}></div>
                         </div>
 
-                        <p>
-                            Uzoma Orji is a creative technologist and visual artist from Owerri, Nigeria.
-                        </p>
-                        <p>
-                            As a technologist he seeks to design engaging human-centred digital experiences that bring the arts and 
-                            tech into the same conversation.
-                        </p>
-                        <p>
-                            As an artist he observes and then creates representations of society and of history; visual metaphors that explain
-                            his millennial Igbo Nigerian cultural context and the cultural environment he hopes to one day live in.
-                        </p>
+                        {
+                            !isPartyVersion ?
+                                <>
+                                    <p>
+                                        Uzoma Orji is a creative technologist and visual artist from Owerri, Nigeria.
+                                    </p>
+                                    <p>
+                                        As a technologist he seeks to design engaging human-centred digital experiences that bring the arts and 
+                                        tech into the same conversation.
+                                    </p>
+                                    <p>
+                                        As an artist he observes and then creates representations of society and of history; visual metaphors that explain
+                                        his millennial Igbo Nigerian cultural context and the cultural environment he hopes to one day live in.
+                                    </p>
+
+                                    <p>
+                                        The essence of his practice is indeed identity, particularly as pertains to self, culture, and nationality. 
+                                        His work uses his notions of his identity as a point of exit to interrogate who we are, 
+                                        how we have come to be and who we aspire to become. 
+                                    </p>
+
+                                    <p>
+                                        He lives and works in Abuja, Nigeria.
+                                    </p>
+                                </>
+                                :
+                                <>
+                                    <p>
+                                        [someone i've just met]: <span className={aboutStyles.newFriend}>so what do you do?</span>
+                                    </p>
+                                    <p>
+                                        [me]: *sips drink in panic* umm, i'm an artist
+                                    </p>
+                                    <p className={aboutStyles.newFriend}>
+                                        what kind of artist?
+                                    </p>
+                                    <p>
+                                        well I do like these conceptual photoshoots that explore themes i'm interested in. i also make websites
+                                    </p>
+                                    <p className={aboutStyles.newFriend}>
+                                        oh for real?
+                                    </p>
+                                    <p>
+                                        yeah. and apps and games. i'm very interested in interactive tech and design. like tech that's really fun and engaging.
+                                    </p>
+                                    <p className={aboutStyles.newFriend}>
+                                        sounds cool man.
+                                    </p>
+                                    <p>
+                                        yeah, it's fun. there's other stuff but, yeah. how about you?
+                                    </p>
+                                    <p className={aboutStyles.newFriend}>
+                                        I'm a cancer researcher.
+                                    </p>
+                                    <p>
+                                        <span role="img" aria-label="mindblown emoji">🤯</span>
+                                    </p>
+                                </>
+                        }
 
                         <p>
-                            The essence of his practice is indeed identity, particularly as pertains to self, culture, and nationality. 
-                            His work uses his notions of his identity as a point of exit to interrogate who we are, 
-                            how we have come to be and who we aspire to become. 
+                            <button onClick={() => setIsPartyVersion(!isPartyVersion)} className={aboutStyles.toggleBtn}>
+                                { toggleText }
+                            </button>
                         </p>
-
-                        <p>
-                            He lives and works in Abuja, Nigeria.
-                        </p>
-
                     </div>
                 </section>
 
