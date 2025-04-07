a884d83ec0163e69b1bb2567973041464c3418b1
uzomao <chidumaga@gmail.com>
2025-04-07 04:03:40 +0100

    adds social links

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { FaBars, FaTimes, FaRegEnvelope, FaStarOfLife, FaInstagram, FaExternalLinkAlt } from 'react-icons/fa'
+import { FaBars, FaTimes, FaRegEnvelope, FaStarOfLife, FaInstagram, FaBookmark, FaLinkedin } from 'react-icons/fa'
 
 import { Link } from 'gatsby'
 
@@ -114,15 +114,21 @@ export default class Navigation extends React.Component {
             <li><a href='https://uzomas.garden' target='_blank' rel='noopener noreferrer'><span role="img" aria-label='sunflower emoji'>🌻</span> Garden <span role="img" aria-label='sunflower emoji'>🌻</span></a></li>
             
             <span className={navigationStyles.communicationLinks}>
-                <a href="mailto:chidumaga@gmail.com" target="_blank" rel="noopener noreferrer">
+                <a href="mailto:uzoma@uzoma.studio" target="_blank" rel="noopener noreferrer" className={navigationStyles.socialLink}>
                     <li><FaRegEnvelope /></li>
                 </a>
-                <a href="https://instagram.com/uzzzoma" target="_blank" rel="noopener noreferrer">
+                <a href="https://linkedin.com/in/uzomacorji" target="_blank" rel="noopener noreferrer" className={navigationStyles.socialLink}>
+                    <li><FaLinkedin /></li>
+                </a>
+                <a href="https://instagram.com/uzzzoma" target="_blank" rel="noopener noreferrer" className={navigationStyles.socialLink}>
                     <li><FaInstagram /></li>
                 </a>
-                <a href="https://www.are.na/chidumaga-uzoma-orji" target="_blank" rel="noopener noreferrer" style={{fontSize: '18px'}}>
+                {/* <a href="https://www.are.na/chidumaga-uzoma-orji" target="_blank" rel="noopener noreferrer" style={{fontSize: '18px'}} className={navigationStyles.socialLink}>
                     <li><FaStarOfLife /><FaStarOfLife /></li>
                 </a>
+                <a href="https://thegardendispatch.substack.com" target="_blank" rel="noopener noreferrer" style={{fontSize: '18px'}} className={navigationStyles.socialLink}>
+                    <li><FaBookmark /></li>
+                </a> */}
             </span>
         </ul>
 
